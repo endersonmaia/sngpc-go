032fe35e03fbcb1f45061f22d44e08cc66d9995e
Enderson Maia <endersonmaia@gmail.com>
2019-03-14 15:00:22 -0300

    implementado FromXML

--- main.go
+++ main.go
@@ -1,59 +1,17 @@
 package main
 
 import (
-	"bufio"
-	"encoding/xml"
 	"fmt"
-	"os"
-	"strings"
 
 	"github.com/endersonmaia/sngpc-go/sngpc"
-	"golang.org/x/net/html/charset"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
 func main() {
-	xmlFile, err := os.Open("mensagemSNGPCInventario.sample.xml")
-	check(err)
 
-	dec := xml.NewDecoder(xmlFile)
-	dec.CharsetReader = charset.NewReaderLabel
+	mysngpcmov := sngpc.MovimentoFromXMLPath(`mensagemSNGPC.sample.xml`)
+	fmt.Printf("\n%s", &mysngpcmov)
 
-	defer xmlFile.Close()
-
-	_, err = xmlFile.Seek(40, 0)
-	check(err)
-
-	reader := bufio.NewReader(xmlFile)
-	buffer, err := reader.Peek(100)
-	check(err)
-
-	if strings.Contains(string(buffer), `<mensagemSNGPCInventario xmlns="urn:sngpc-schema">`) {
-
-		_, err = xmlFile.Seek(0, 0)
-		check(err)
-
-		mysngpc := sngpc.MensagemSNGPCInventario{}
-
-		err = dec.Decode(&mysngpc)
-		check(err)
-
-		fmt.Printf("\n%s", mysngpc)
-	} else if strings.Contains(string(buffer), `<mensagemSNGPC xmlns="urn:sngpc-schema">`) {
-		_, err = xmlFile.Seek(0, 0)
-		check(err)
-
-		mysngpc := sngpc.MensagemSNGPC{}
-
-		err = dec.Decode(&mysngpc)
-		check(err)
-
-		fmt.Printf("\n%s", mysngpc)
-	}
+	mysngpcinv := sngpc.InventarioFromXMLPath(`mensagemSNGPCInventario.sample.xml`)
+	fmt.Printf("\n%s", &mysngpcinv)
 
 }
--- sngpc/sngpc.go
+++ sngpc/sngpc.go
@@ -1,5 +1,61 @@
 package sngpc
 
+import (
+	"encoding/xml"
+	"os"
+
+	"golang.org/x/net/html/charset"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 // http://portal.anvisa.gov.br/sngpc/desenvolvedores
 
 const SNGPCXMLVersion = "2.0"
+
+type TipoMensagemSNGPC uint8
+
+const (
+	Inventario TipoMensagemSNGPC = iota
+	Movimentacao
+)
+
+func InventarioFromXMLPath(f string) MensagemSNGPCInventario {
+
+	xmlFile, err := os.Open(f)
+	check(err)
+
+	dec := xml.NewDecoder(xmlFile)
+	dec.CharsetReader = charset.NewReaderLabel
+
+	defer xmlFile.Close()
+
+	mysngpc := MensagemSNGPCInventario{}
+
+	err = dec.Decode(&mysngpc)
+	check(err)
+
+	return mysngpc
+}
+
+func MovimentoFromXMLPath(f string) MensagemSNGPC {
+
+	xmlFile, err := os.Open(f)
+	check(err)
+
+	dec := xml.NewDecoder(xmlFile)
+	dec.CharsetReader = charset.NewReaderLabel
+
+	defer xmlFile.Close()
+
+	mysngpc := MensagemSNGPC{}
+
+	err = dec.Decode(&mysngpc)
+	check(err)
+
+	return mysngpc
+}
